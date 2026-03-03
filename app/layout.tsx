a862bd8bb7b40cdebfcb4b7e6a486724f9410a6f
irfandwihs <irfandwi.hs@gmail.com>
2026-03-03 21:08:16 +0700

    add analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,27 @@
-import type {Metadata} from 'next';
-import './globals.css'; // Global styles
-import { AuthProvider } from '@/contexts/AuthContext';
+import type { Metadata } from "next";
+import "./globals.css"; // Global styles
+import { AuthProvider } from "@/contexts/AuthContext";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-  title: 'AksaraPlay - Petualangan Belajar Seru!',
-  description: 'AksaraPlay adalah platform game edukasi interaktif yang menggabungkan warisan budaya dengan keseruan bermain.',
+  title: "AksaraPlay - Petualangan Belajar Seru!",
+  description:
+    "AksaraPlay adalah platform game edukasi interaktif yang menggabungkan warisan budaya dengan keseruan bermain.",
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className="bg-slate-50 text-slate-900 min-h-screen">
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+      <body
+        suppressHydrationWarning
+        className="bg-slate-50 text-slate-900 min-h-screen"
+      >
+        <AuthProvider>{children}</AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
